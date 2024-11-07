69d9f3050a2837d520abb249645129ae96d450d9
xiao-mao88 <laurenli045@gmail.com>
2024-11-07 11:23:07 -0500

    added a controller

--- src/cs3500/threetrios/Controller/Controller.java
+++ src/cs3500/threetrios/Controller/Controller.java
@@ -1,4 +1,18 @@
 package cs3500.threetrios.Controller;
 
+/**
+ * Controller interface that takes in the user's inputs, feeding to the model and
+ * allowing the user to play the game.
+ */
 public interface Controller {
+
+  /**
+   *
+   */
+  void playGame();
+
+  /**
+   *
+   */
+  void handleCellClick();
 }
---
+++ src/cs3500/threetrios/Controller/ThreeTriosSwingController.java
@@ -0,0 +1,21 @@
+package cs3500.threetrios.Controller;
+
+import cs3500.threetrios.model.Model;
+
+public class ThreeTriosSwingController implements Controller {
+  private Model model;
+
+  public ThreeTriosSwingController() {
+
+  }
+
+  @Override
+  public void playGame() {
+
+  }
+
+  @Override
+  public void handleCellClick() {
+
+  }
+}
